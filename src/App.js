94cb83ace3dcab8cdc91b2d3a0349c65012a19bd
wjheo <91wjheo@gmail.com>
2020-05-06 01:27:07 +0900

    upload movie app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
 import React from 'react';
-import logo from './logo.svg';
+import PropTypes from "prop-types";
+import axios from 'axios';
+import Movie from './Movie';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    state = {
+        isLoading: true,
+        movies: []
+    };
+
+    // setState()를 호출할 때 마다, 새로운 state 와함께 render()함수가 호출된다.
+    // 따라서 화면에는 변경된 state 값이 동적으로 보여지게 된다.
+    constructor(props) {
+        super(props);
+    }
+
+    async getMovies() {
+        // api 에서 movie 리스트 가져옴.
+        const {data: {data: {movies}}} = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_by=rating');
+        // api에서 가져온 movie 리스트로 state 데이터를 갱신함.
+        this.setState({movies, isLoading: false});
+
+        console.log("getMovies() : ", movies);
+    }
+
+    componentDidMount() {
+        this.getMovies();
+    }
+
+    // React는 react component class의 render 메소드를 자동으로 호출한다.
+    render() {
+        const {isLoading, movies} = this.state; //  이렇게 하면 this.state안에 있는 key isLoading, movies에 해당하는 value가 각각 const변수에 할당된다.
+        return (
+            <section className="container">
+                {isLoading
+                    ? (<div className="loader">
+                        <span className="loader__text">
+                            is Loading...
+                        </span>
+                      </div>)
+                    : (
+                        <div className="movies">
+                            {movies.map(movie => {
+                            return (<Movie key={movie.id}
+                                           id={movie.id}
+                                           title={movie.title}
+                                           year={movie.year}
+                                           summary={movie.summary}
+                                           poster={movie.medium_cover_image}
+                                           rating={movie.rating}
+                                           genres={movie.genres.splice(0,3)}
+                                    />)})}
+                        </div>
+
+                      )
+                }
+            </section>
+        )
+    }
+
+    componentDidUpdate() {
+    }
+
 }
+
+
+// function App() {
+//   return (
+//       <div>
+//           {foodILike.map(food => (<Food key ={food.id} fav={food.name} img={food.img} rating={food.rating} />))}
+//       </div>
+//   )
+// }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+// <APP /> 은 html이 아니다. react의 component라는 것인데, 이건 html을 반환하는 함수이다.
+// 이런걸 jsx 라고 하는데, jsx는 javascript안의 html이다.
+// 그리고 react application은 한 번에 하 나의 component만 렌더링 할 수 있다.
+// 따라서 모든 컴포넌트들은 App.js 안에 들어가야한다.
+ReactDOM.render(<App />, document.getElementById('root'));
